8265e2835387d62a68df40a02000c36173f5b32f
Abishan <abishanjeyarajah@gmail.com>
2025-07-29 15:22:20 +0530

    added new tables

--- abishan/app/Models/Employee.php
+++ abishan/app/Models/Employee.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = ['first_name', 'last_name'];
+
+    public function phones()
+    {
+        return $this->hasOne(Phone::class);
+    }
 }
